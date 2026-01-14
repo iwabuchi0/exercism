f7c4e933a51972293282d9e6c8ce4122850e0cc2
iwabuchi0 <199025978+iwabuchi0@users.noreply.github.com>
2026-01-14 14:24:27 -0300

    feat: Solve isogram exercise

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,2 +1,11 @@
 def translate(text):
-    pass
+    rule1 = "xr", "yt","a", "e", "i", "o", "u"
+    rule2 = "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"
+    for item in text:
+        if item.startswith(rule2):
+           item.strip(rule2)
+           item = item + "ay"
+           return item
+        if item.startswith(rule1):
+            item += "ay"
+            return item         
