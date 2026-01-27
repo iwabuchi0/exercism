4a3a6a2a6bb27e0d654a21917387d52e4221e61e
iwabuchi0 <199025978+iwabuchi0@users.noreply.github.com>
2026-01-27 16:12:41 -0300

    wip: basic key extraction logic implemented

---
+++ rotational-cipher/rotational_cipher.py
@@ -0,0 +1,12 @@
+def rotate(text, key):
+    rot = 0
+    if key[-1].isdigit() and key[-2].isdigit()
+        rot = int(key[-2:])
+        return rot
+    if key[-1].isdigit():
+        rot = int(key[-1])
+        return rot
+    for i in range(len(text) + 1):
+
+
+#para cada item, a gente soma com o rot que descobrimos 
