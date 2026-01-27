a0b6975b67b0b7f2ef7af79f98e61f1a81fab59c
iwabuchi0 <199025978+iwabuchi0@users.noreply.github.com>
2026-01-26 23:32:14 -0300

    feat: implement aliquot and sum and perfect number classification

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,6 +1,6 @@
 import math
 
-def is_prime(number)
+def is_prime(number):
     if number <= 1:
         return False
     if number <= 3:
@@ -10,6 +10,24 @@ def is_prime(number)
     for i in range (5, math.isqrt(number) + 1, 6):
         if number % i == 0 or number % (i+2) == 0:
             return False
+    return True 
+        
+def aliquot_sum(number):
+    if number == 1:
+        return 0
+    if is_prime(number):
+        return 1
+    soma = 1
+    root = math.isqrt(number)
+
+    for i in range(2, root + 1):
+        if number % i == 0:
+            soma += i 
+            par = number // i
+            if par != i:
+                soma += par
+    return soma
+    
 
 def classify(number):
     """ A perfect number equals the sum of its positive divisors.
@@ -17,8 +35,18 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    if number <= 3 OR :
-        return "deficient"
     if number <= 0: 
         raise ValueError("Classification is only possible for positive integers.")
-    if number 
+    if number == 1:
+        return "deficient"
+    
+    result_some = aliquot_sum(number)
+
+    if is_prime(number):
+        return "deficient"
+    if result_some == number:
+        return "perfect"
+    if result_some > number:
+        return "abundant" 
+    else: 
+        return "deficient"
