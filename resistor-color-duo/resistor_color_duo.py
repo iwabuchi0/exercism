6414dd28677bb3c41d3867e429211691cf5bdf44
iwabuchi0 <199025978+iwabuchi0@users.noreply.github.com>
2026-01-28 20:57:33 -0300

    feat: implement initial functional logic for resistor-color-duo

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,8 @@
 def value(colors):
-    pass
+    resistor_color_list = ['black','brown','red','orange','yellow','green','blue','violet','grey','white']
+    a = []
+    
+    for i in colors[0:2]:
+        number_color = str(resistor_color_list.index(colors[0])) + str(resistor_color_list.index(colors[1]))
+        return int(number_color)
+          
