a2715060541da0280865f680029e2c72152c67a2
iwabuchi0 <199025978+iwabuchi0@users.noreply.github.com>
2026-01-29 15:26:24 -0300

    wip: initial implementation of Pig Latin translation logic

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,11 +1,19 @@
 def translate(text):
     rule1 = "xr", "yt","a", "e", "i", "o", "u"
     rule2 = "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"
+    rule3 = "qu"
+    rule4 = "y"
+    new_phrase = []
     for item in text:
-        if item.startswith(rule2):
-           item.strip(rule2)
-           item = item + "ay"
-           return item
-        if item.startswith(rule1):
-            item += "ay"
-            return item         
+        if item[0] == 'q' and item[1] == 'u':
+            new_world = item[2:] + "quay"
+            new_phrase.append(new_world)
+            continue
+        if item[0:2] == 'xr' or item[0:2] == 'yt' or item[0] in rule1:
+            new_world = item + "ay"
+            new_phrase.append(new_world)
+            continue
+        if item[0] == 'y' or item[1] == 'y' or item[2] == 'y':
+            new_world = item[1:] + item[0] + "ay"
+            new_phrase.append(new_world)
+            continue    
