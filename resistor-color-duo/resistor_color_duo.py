630508ea94d37446989495aec508c7d4ca445fed
iwabuchi0 <199025978+iwabuchi0@users.noreply.github.com>
2026-01-28 21:01:31 -0300

    refactor: optimize color-to-value logic using arithmetic instead of strings

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,8 +1,7 @@
+RESISTOR_COLORS_LIST = ['black','brown','red','orange','yellow','green','blue','violet','grey','white']
 def value(colors):
-    resistor_color_list = ['black','brown','red','orange','yellow','green','blue','violet','grey','white']
-    a = []
     
-    for i in colors[0:2]:
-        number_color = str(resistor_color_list.index(colors[0])) + str(resistor_color_list.index(colors[1]))
-        return int(number_color)
+    first_digit = RESISTOR_COLORS_LIST.index(colors[0]) * 10
+    second_digit = RESISTOR_COLORS_LIST.index(colors[1])
+    return first_digit + second_digit
           
