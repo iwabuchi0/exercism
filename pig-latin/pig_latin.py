c2291a8c8986f52fe05a41b2b0473846de2f6186
iwabuchi0 <199025978+iwabuchi0@users.noreply.github.com>
2026-02-03 15:02:17 -0300

    feat: implement pig latin translation logic

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -11,6 +11,20 @@ def translate(text):
         if word.startswith(RULE1):
             new_phrase.append(word + 'ay')
             continue
+        elif word.startswith(RULE3):
+            new_phrase.append(word[2:] + "quay")
+        elif word [1:3] == "qu":
+            new_phrase.append(word[3:] + word[:3] + "ay")
+        elif word[1] == "y":
+            new_phrase.append(word[1:] + word[0] + "ay")
+        elif word[2] == "y":
+            new_phrase.append(word[2:] + word[0:2] + "ay")
+        elif word[0] in RULE2 and word[1] in RULE2 and word[2] in RULE2:
+            new_phrase.append(word[3:] + word[0:3] + "ay")
+        elif word[0] in RULE2 and word[1] in RULE2:
+            new_phrase.append(word[2:] + word[0:2] + "ay")
+        elif word.startswith(RULE2):
+            new_phrase.append(word[1:] + word[0] + "ay")
     return ' '.join(new_phrase)
 
 
