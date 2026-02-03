8c0557a50cc885f7034fce4ac7696589e44fe6b5
iwabuchi0 <199025978+iwabuchi0@users.noreply.github.com>
2026-02-03 13:40:25 -0300

    wip: initial Pig Latin structure with words list and Rule 1

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,19 +1,16 @@
+RULE1 = ("xr", "yt","a", "e", "i", "o", "u")
+RULE2 = ("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z")
+RULE3 = ("qu")
+RULE4 = ("y")
+
 def translate(text):
-    rule1 = "xr", "yt","a", "e", "i", "o", "u"
-    rule2 = "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"
-    rule3 = "qu"
-    rule4 = "y"
+    words_list = text.split()
     new_phrase = []
-    for item in text:
-        if item[0] == 'q' and item[1] == 'u':
-            new_world = item[2:] + "quay"
-            new_phrase.append(new_world)
+
+    for word in words_list:
+        if word.startswith(RULE1):
+            new_phrase.append(word + 'ay')
             continue
-        if item[0:2] == 'xr' or item[0:2] == 'yt' or item[0] in rule1:
-            new_world = item + "ay"
-            new_phrase.append(new_world)
-            continue
-        if item[0] == 'y' or item[1] == 'y' or item[2] == 'y':
-            new_world = item[1:] + item[0] + "ay"
-            new_phrase.append(new_world)
-            continue    
+    return ' '.join(new_phrase)
+
+
